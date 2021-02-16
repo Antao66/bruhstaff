047092b8dba8faf31ef3e5adfeaaacc6dd61c1c3
Antao66 <46621433+Antao66@users.noreply.github.com>
2021-02-16 16:39:45 +0200

    add fugas lab 2

---
+++ Fugas 2.4/Fugas 2.4/Fugas 2.4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num, simp = 2;
+	bool isPrime = false;
+	cout << "Enter a number: ";
+	cin >> num;
+	while (num >= 3 && isPrime == false)
+	{
+		num--;
+		for (int i = 2; i <= num / 2; i++)
+		{
+			if (num % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+			isPrime = true;
+			simp = num;
+		}
+	}
+	cout << "\nPrevious simple number is: " << simp << endl;
+}
